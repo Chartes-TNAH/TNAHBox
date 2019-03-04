a3455e40e4d13f493771fc5971cc550707a5138a
segolene <segolene.albouy@gmail.com>
2019-03-05 00:33:02 +0100

    Ajout de commentaires sur les constantes utilisées par l'application

--- app/constantes.py
+++ app/constantes.py
@@ -6,18 +6,24 @@ from warnings import warn
 # CONSTANTE2 = 2
 
 SECRET_KEY = "JE SUIS UN SECRET !"
+# variable nécessaire à la création d'applications Flask
+# utilisée comme clé cryptographique pour générer des tokens notamment
+# elle permet avec Flask-WTForm de protéger l'envoi des données
+# lorsque l'utilisateur soumet des formulaires web
 API_ROUTE = "/api"
 
 if SECRET_KEY == "JE SUIS UN SECRET !":
     warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
 
+# Les paramètres de configuration de l'application en mode test
+# sont définis en tant que variables de classe au sein de la classe _TEST
 class _TEST:
     SECRET_KEY = SECRET_KEY
     # On configure la base de données de test
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.sqlite'
 # sqlite:// représente le moteur utilisé, ici SQLite
 # puis / signifie ici qu'il s'agit d'un chemin relatif (un chemin absolu avec //)
-# puis le chemin qui dit qu'on veut aller chercher, en l'occurence db.sqlite
+# puis le chemin du fichier à aller chercher, en l'occurence db.sqlite
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
