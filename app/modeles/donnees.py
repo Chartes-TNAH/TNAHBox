99383418cd9e3c9c74d203a6d8ae4826db5ab93e
Victoria <victoria.lefourner@gmail.com>
2019-03-11 17:23:06 +0100

    Recherche SQLAlchemy pour BDD

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -5,10 +5,9 @@ from .. app import db
 
 # Convention : les classes portent un nom commençant par une majuscule
 
-# on crée notre modèle pour l'assignation d'un auteur
 class Authorship(db.Model):
-    # __tablename__ = "authorship"
-    # authorship_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    __tablename__ = "authorship"
+     authorship_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
     # authorship_place_id = db.Column(db.Integer, db.ForeignKey('place.place_id'))
     # authorship_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     # authorship_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
