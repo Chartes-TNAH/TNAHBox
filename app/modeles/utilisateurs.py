67965bd37c0ef73ee072e9282c3119cf57c91d04
Lauryne <lemosquet@hotmail.fr>
2019-03-21 19:28:11 +0100

    formulaire d'édition des données utilisateur

--- app/modeles/utilisateurs.py
+++ app/modeles/utilisateurs.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 from app.modeles.donnees import Person
 
 #Création d'un classe pour le formulaire de connexion des utilisateurs. J'utilise
@@ -8,7 +8,7 @@ from app.modeles.donnees import Person
 # correspondent aux données attendues
 # DataRequired = indique que le champ ne peut pas être laissé vide par l'utilisateur
 class LoginForm(FlaskForm):
-    person_login = StringField('Nom utilisateur', validators=[DataRequired()])
+    person_login = StringField('Nom d\'utilisateur', validators=[DataRequired()])
     person_password = PasswordField('Mot de passe', validators=[DataRequired()])
     remember_me = BooleanField('Se souvenir de moi')
     submit = SubmitField('Connexion')
@@ -16,16 +16,16 @@ class LoginForm(FlaskForm):
 # Même chose pour la classe Registration = founit un modèle de formulaire pour
 # l'enregistrement d'un nouvel utilisateur
 class RegistrationForm(FlaskForm):
-    person_firstName = StringField('Prénom<span style="color: red;">*</span>', validators=[DataRequired()])
-    person_name = StringField('Nom<span style="color: red;">*</span>', validators=[DataRequired()])
-    person_login = StringField('Nom d\'utilisateur<span style="color: red;">*</span>', validators=[DataRequired()])
-    person_email = StringField('Email<span style="color: red;">*</span>', validators=[DataRequired(), Email()])
-    person_password= PasswordField('Mot de passe<span style="color: red;">*</span>', validators=[DataRequired()])
-    password2 = PasswordField('Mot de passe<span style="color: red;">*</span>', validators=[DataRequired(), EqualTo('person_password')])
+    person_firstName = StringField('Prénom<span style="color: #b21e1e;">*</span>', validators=[DataRequired()])
+    person_name = StringField('Nom<span style="color: #b21e1e;">*</span>', validators=[DataRequired()])
+    person_login = StringField('Nom d\'utilisateur<span style="color: #b21e1e;">*</span>', validators=[DataRequired()])
+    person_email = StringField('Email<span style="color: #b21e1e;">*</span>', validators=[DataRequired(), Email()])
+    person_password= PasswordField('Mot de passe<span style="color: #b21e1e;">*</span>', validators=[DataRequired()])
+    password2 = PasswordField('Mot de passe<span style="color: #b21e1e;">*</span>', validators=[DataRequired(), EqualTo('person_password')])
     # password2 permet de vérifier le mot de passe pour éviter les erreurs de frappe
     person_git = StringField('Compte Github (URL)')
     person_linkedIn = StringField('Compte LinkedIn (URL)')
-    person_promotion = StringField('Promotion TNAH (année)')
+    person_promotion = StringField('Promotion TNAH (année du diplôme)')
     submit = SubmitField('S\'enregistrer')
 
     def validate_username(self, person_login):
@@ -37,4 +37,13 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Adresse mail déjà enregistrée')
 
-
+class EditProfileForm(FlaskForm):
+    person_login = StringField('Nom d\'utilisateur', validators=[DataRequired()])
+    person_email = StringField('Email', validators=[DataRequired(), Email()])
+    person_name = StringField('Nom', validators=[DataRequired()])
+    person_firstName = StringField('Prénom', validators=[DataRequired()])
+    person_promotion = StringField('Promotion (année du diplôme)')
+    person_git = StringField('Compte GitHub (URL)')
+    person_linkedIn= StringField('Compte LinkedIn (URL)')
+    person_description = TextAreaField('Description')
+    submit = SubmitField('Enregistrer les modifications')
