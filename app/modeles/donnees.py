62a10841d39306428561d717c8349562c172c717
Segolene <segolene.albouy@gmail.com>
2019-03-25 11:43:35 +0100

    Ajout d'une fonction permettant d'enlever un document de ses favoris

--- app/modeles/donnees.py
+++ app/modeles/donnees.py
@@ -237,6 +237,34 @@ class Person(UserMixin, db.Model):
 
         return erreurs
 
+    def remove_docu_to_favorites(user, docu):
+        '''
+        Fonction qui permet d'enlever aux favoris de l'utilisateur
+        le document courant
+        :param user: auteur du document (entrée de la BDD)
+        :param docu: document importé par l'utilisateur (entrée de la BDD)
+        :return: liste d'erreurs d'il y en a
+        '''
+        erreurs = []
+        if not user:
+            erreurs.append("Il n'y a pas d'utilisateur à dissocier")
+        if not docu:
+            erreurs.append("Il n'y a pas de document à dissocier")
+
+        if user is None or docu is None:
+            # si les paramètres renseignés ne correspondent à rien, je ne fais rien
+            return
+
+        if docu in user.faved_document:
+            # si le document est déjà dans la liste des documents favoris de l'utilisateur
+            user.faved_document.remove(docu)
+            # je le supprime de cette liste
+
+        db.session.add(user)
+        db.session.commit()
+
+        return erreurs
+
     def __repr__(self):
         return '<User {}>'.format(self.person_login)
 
