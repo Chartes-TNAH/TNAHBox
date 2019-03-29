590a348cd38bbcca1ef4c874e2d62ae7a8b9c88e
Segolene <segolene.albouy@gmail.com>
2019-03-29 08:14:55 +0100

    Retrait de l'argument donné pour config_app()

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app.app import config_app
 # du fichier app.py dans le dossier app j'importe la fonction config_app
 
 if __name__ == "__main__":
-    app = config_app("production")
+    app = config_app()
     app.run(debug=True)
