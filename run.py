07b4627d3d8fb05454a4960c879d14c1dda8fec0
Segolene <segolene.albouy@gmail.com>
2019-03-29 11:44:05 +0100

    Retrait de Track modification de SQLAlchemy dans la configuration de l'app

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ import os
 
 if __name__ == "__main__":
     app = app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app = app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.run(debug=True)
